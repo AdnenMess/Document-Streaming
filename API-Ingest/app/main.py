d5cf69c4416cf207f6842da5493e8cb793eaf293
AdnenMess <adnen_messaoudi@hotmail.com>
2023-03-14 16:31:12 +0100

    Update Client Folder

--- API-Ingest/app/main.py
+++ API-Ingest/app/main.py
@@ -1,6 +1,6 @@
 # You need this to use FastAPI, work with statuses and be able to end HTTPExceptions
 from fastapi import FastAPI, status, HTTPException
- 
+
 # You need this to be able to turn classes into JSONs and return
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
@@ -15,9 +15,8 @@ from datetime import datetime
 from kafka import KafkaProducer, producer
 
 
-
 # Create class (schema) for the JSON
-# Date get's ingested as string and then before writing validated
+# Date gets ingested as string and then before writing validated
 class InvoiceItem(BaseModel):
     InvoiceNo: int
     StockCode: str
@@ -28,17 +27,20 @@ class InvoiceItem(BaseModel):
     CustomerID: int
     Country: str
 
+
 # This is important for general execution and the docker later
 app = FastAPI()
+
 
 # Base URL
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 # Add a new invoice
 @app.post("/invoiceitem")
-async def post_invoice_item(item: InvoiceItem): #body awaits a json with invoice item information
+async def post_invoice_item(item: InvoiceItem):  # body awaits a json with invoice item information
     print("Message received")
     try:
         # Evaluate the timestamp and parse it to datetime object you can work with
@@ -50,30 +52,31 @@ async def post_invoice_item(item: InvoiceItem): #body awaits a json with invoice
         # Use strftime to parse the string in the right format (replace / with - and add seconds)
         item.InvoiceDate = date.strftime("%d-%m-%Y %H:%M:%S")
         print("New item date:", item.InvoiceDate)
-        
+
         # Parse item back to json
         json_of_item = jsonable_encoder(item)
-        
+
         # Dump the json out as string
         json_as_string = json.dumps(json_of_item)
         print(json_as_string)
-        
+
         # Produce the string
         produce_kafka_string(json_as_string)
 
         # Encode the created customer item if successful into a JSON and return it to the client with 201
         return JSONResponse(content=json_of_item, status_code=201)
-    
+
     # Will be thrown by datetime if the date does not fit
     # All other value errors are automatically taken care of because of the InvoiceItem Class
     except ValueError:
         return JSONResponse(content=jsonable_encoder(item), status_code=400)
-        
+
 
 def produce_kafka_string(json_as_string):
     # Create producer
-        producer = KafkaProducer(bootstrap_servers='kafka:9092',acks=1)
-        
-        # Write the string as bytes because Kafka needs it this way
-        producer.send('ingestion-topic', bytes(json_as_string, 'utf-8'))
-        producer.flush() 
+    # producer = KafkaProducer(bootstrap_servers='localhost:9093', acks=1) # to test first in local machine
+    producer = KafkaProducer(bootstrap_servers='kafka:9092', acks=1)
+
+    # Write the string as bytes because Kafka needs it this way
+    producer.send('ingestion-topic', bytes(json_as_string, 'utf-8'))
+    producer.flush()
